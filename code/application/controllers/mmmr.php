e5cde7dd5abfd11a712f69ac5991457ad4fd4a32
Lei Wang <leiw414@gmail.com>
2014-06-24 11:17:00 -0400

    Update Restful API---MMMR Controller
    Update Restful API---MMMR Controller

--- code/application/controllers/mmmr.php
+++ code/application/controllers/mmmr.php
@@ -1,5 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+/*-----Phase2 Restful API for Statistic Library ---*/
+
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
 require APPPATH.'/libraries/REST_Controller.php';
 
